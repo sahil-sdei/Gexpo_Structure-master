b2b09b7690bda9f006cee952bffafaaed9f781cb
sahilsa <sahilsa@smartdatainc.net>
2018-03-30 12:01:19 +0530

    Custom Fonts

---
+++ app/src/main/java/ggn/home/help/utils/widgets/CustomTextView.java
@@ -0,0 +1,46 @@
+package ggn.home.help.utils.widgets;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import ggn.home.help.R;
+
+public class CustomTextView extends TextView {
+
+    public CustomTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs);
+    }
+
+    public CustomTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs);
+
+    }
+
+    public CustomTextView(Context context) {
+        super(context);
+        init(null);
+    }
+
+    private void init(AttributeSet attrs) {
+        if (attrs != null) {
+            TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.CustomTextView);
+            String fontName = a.getString(R.styleable.CustomTextView_fontG);
+
+            try {
+                if (fontName != null) {
+                    Typeface myTypeface = Typeface.createFromAsset(getContext().getAssets(), "fonts/" + fontName);
+                    setTypeface(myTypeface);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            a.recycle();
+        }
+    }
+}
