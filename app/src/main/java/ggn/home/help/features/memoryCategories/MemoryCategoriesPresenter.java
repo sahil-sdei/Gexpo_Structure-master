bb4bedf49ae97acad041d32340ae535c8a7a4843
sahilsa <sahilsa@smartdatainc.net>
2018-04-03 17:46:02 +0530

    Comments changes

--- app/src/main/java/ggn/home/help/features/memoryCategories/MemoryCategoriesPresenter.java
+++ app/src/main/java/ggn/home/help/features/memoryCategories/MemoryCategoriesPresenter.java
@@ -1,12 +1,16 @@
 package ggn.home.help.features.memoryCategories;
 
 
+import ggn.home.help.features.fullLifeAlbum.FullLifeAlbumActivity;
 import ggn.home.help.features.internal.base.BasePresenter;
 
 public class MemoryCategoriesPresenter extends BasePresenter<MemoryCategoriesView> implements MemoryCategoriesAdapterBinder {
 
     @Override
     public void onItemClicked(Categories categories) {
-        getView().goToAddMemories(categories);
+        if (categories.title.equalsIgnoreCase("Full Life Album"))
+            FullLifeAlbumActivity.start(getView().getActivityG());
+        else
+            getView().goToAddMemories(categories);
     }
 }
